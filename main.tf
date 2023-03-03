d041b507e9a07ba4a972a69a3105163a175cdf2c
AhmedMSharaf <55796502+AhmedMSharaf@users.noreply.github.com>
2023-03-03 21:57:37 +0200

    update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,61 @@
 
-resource "aws_instance" "my-ec2" {
-  ami           = "ami-0c0933ae5caf0f5f9"
-  instance_type = "t2.micro"
-  key_name      = "general key.pem"
-  associate_public_ip_address = true
-
-  vpc_security_group_ids = ["${aws_security_group.sharaf-sec.id}"]
-  subnet_id              = "${aws_subnet.private-subnet.id}"
-
-}
-
-resource "aws_security_group" "sharaf-sec" {
-  name_prefix = "sharaf-sec"
-  
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_subnet" "private-subnet" {
-  vpc_id     = "${aws_vpc.vpc-ahmed-sharaf.id}"
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_vpc" "vpc-ahmed-sharaf" {
+resource "aws_vpc" "vpc_sharaf" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc-sharaf"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.vpc_sharaf.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+  tags = {
+    Name = "public subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc_sharaf.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-central-1a"
+  tags = {
+    Name = "private subnet"
+  }
+}
+
+# Define the security group
+resource "aws_security_group" "sharaf_security" {
+  name_prefix = "sharaf-security"
+  vpc_id = aws_vpc.vpc_sharaf.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sharaf-security"
+  }
+}
+
+# Define the EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami = "ami-0c0933ae5caf0f5f9"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.sharaf_security.id]
+  tags = {
+    Name = "ec2-instance"
+  }
 }
